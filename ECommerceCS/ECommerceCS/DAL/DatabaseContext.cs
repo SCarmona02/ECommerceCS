4ab79bc99a9e7a36a619305faee9e227163a6cf0
SCarmona02 <santiago.car2003@hotmail.com>
2023-03-07 18:28:31 -0500

    Create proyect in Entity Framework, create new entity Country and use DatabaseContext in program

---
+++ ECommerceCS/ECommerceCS/DAL/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceCS.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceCS.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
